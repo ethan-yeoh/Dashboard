6e9d9051af3e16ad5a591eb2c3eb3d961b128ba4
ethan-yeoh <107853055+ethan-yeoh@users.noreply.github.com>
2025-09-07 22:52:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_SUPABASE_ANON_KEY"
+  "https://fcascylnltjslnwsxtpv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjYXNjeWxubHRqc2xud3N4dHB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyNTM4MTMsImV4cCI6MjA3MjgyOTgxM30.3HrL7XCkhlef8Y7724g4dOCBklLPzOPsH5Gkw4ghKNs"
 );
+
